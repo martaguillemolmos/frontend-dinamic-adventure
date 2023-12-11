b747e3256edf9f84a0234a1261367a15830966f1
martaguillemolmos <martetaguillem@gmail.com>
2023-12-11 21:05:31 +0100

    Feat: Made a new logo design, modified the search engine logo and started designing the header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Header } from "./common/Header/Header";
 import { Register } from "./pages/Register/Register";
 
 function App() {
   return (
     <>
+      <Header />
       <Register />
     </>
   );
---
+++ src/common/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css"
+import logoImage from "../../img/Logo.png"
+export const Header = () => {
+    return (
+        <div className="headerDesign">
+                <div className="image-logo">
+                    <img src ={logoImage} alt="Logo"/></div>
+                <div className="elements-nav"> Header</div>
+        </div>
+    )}
