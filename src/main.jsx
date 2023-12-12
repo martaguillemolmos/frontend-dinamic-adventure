d68e0e70763589d87490e52ef6dad430fe347c34
martaguillemolmos <martetaguillem@gmail.com>
2023-12-12 10:21:52 +0100

    Feat: Wrap application with Redux Persiste.

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,22 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
 
+//REDUX PERSISTENCE
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-        <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
