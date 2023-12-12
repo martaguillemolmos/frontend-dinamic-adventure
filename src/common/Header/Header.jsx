f3b52369ab4ac65ffed061b81dfe239471c50d9d
martaguillemolmos <martetaguillem@gmail.com>
2023-12-12 13:46:18 +0100

    Feat: Add functionalities to the header: - In the case that we have a token stored in redux we will display elements of the header or others. - In the event that the user has a token stored in redux, the option to log in or register will no longer appear in the header, instead the Profile and the logout button will appear. - By clicking on the logout button, the token stored in redux is deleted and we display the Register and Log in elements.

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -2,22 +2,54 @@ import "./Header.css";
 import logoImage from "../../img/Logo.png";
 import { LinkButton } from "../LinkButton/LinkButton";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, userData } from "../../pages/userSlice";
 
 export const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const rdxToken = useSelector(userData);
+  // console.log("este token es lo mejor", rdxToken);
+
+  const dispatch = useDispatch();
+
+  const logOutMe = () => {
+    dispatch(logout({ credentials: "" }));
+    navigate("/");
+  };
 
   return (
     <div className="headerDesign">
+      {/* Vistas públicas */}
       <div className="image-logo" onClick={() => navigate("/")}>
         <img src={logoImage} alt="Logo" />
       </div>
       <div className="elements-nav">
-        <LinkButton path={"/actividad-acuatica"} title={"Actividades acuáticas"} />
-        <LinkButton path={"/actividad-terrestre"} title={"Actividades terrestres"} />
+        <LinkButton
+          path={"/actividad-acuatica"}
+          title={"Actividades acuáticas"}
+        />
+        <LinkButton
+          path={"/actividad-terrestre"}
+          title={"Actividades terrestres"}
+        />
         <LinkButton path={"/contacto"} title={"Contacto"} />
-        <LinkButton path={"/registro"} title={"Registarse"} />
-        <div className="buttonSession" onClick={() => navigate("/login")}> Inicia sesión </div>        
-        {/* <div className="buttonSession" onClick={() => navigate("/")}> RESERVA </div>         */}
+        {/* Vistas si tienes o no token almacenado en Redux*/}
+        {/* No tienes token almacenado en Redux*/}
+        {rdxToken.credentials == "" ? (
+          <>
+            <LinkButton path={"/registro"} title={"Registarse"} />
+            <div className="buttonSession" onClick={() => navigate("/login")}>
+              Inicia sesión
+            </div>
+          </>
+        ) : (
+          <>
+            <LinkButton path={"/profile"} title={"Perfil"} />
+             <div className="buttonSession" onClick={logOutMe} path={"/"}>
+              Cerrar sesión
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
