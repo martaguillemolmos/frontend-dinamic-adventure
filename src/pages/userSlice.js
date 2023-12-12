c142cd81f52590a01bbe357a15de2a0a797ac797
martaguillemolmos <martetaguillem@gmail.com>
2023-12-12 10:10:17 +0100

    Feat: Create the userSlice.js document, to do this, we install redux toolkit in the project. Finally, we create the app folder inside src and it will contain our storage.

---
+++ src/pages/userSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+      credentials: {
+      token: "",
+      name: ""
+      }
+    },
+    reducers: {
+      login: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      },
+      logout: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      }
+      
+    } 
+    
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const userData = (state) => state.user;
+
+export default userSlice.reducer;
---
+++ src/services/apiCalls.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const hostURL = "http://localhost:4000"
+
+//Login
+export const loginUser = async (body) => {
+    return await axios.post (`${hostURL}/user`, body) 
+}
