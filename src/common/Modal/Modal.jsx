6013c38fa4327f85d3691e22f0eeef4a761d4df6
martaguillemolmos <martetaguillem@gmail.com>
2023-12-21 11:27:43 +0100

    Feat: Retrieve the appointment information in the modal and allow the user to modify both the date and the number of participants. In addition to being able to cancel the activity.

--- src/common/Modal/Modal.jsx
+++ src/common/Modal/Modal.jsx
@@ -2,72 +2,209 @@ import "./Modal.css";
 import { updateAppointment } from "../../services/apiCalls";
 import { useSelector } from "react-redux";
 import { userData } from "../../pages/userSlice";
+import { CustomInput } from "../CustomInput/CustomInput";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
 
 const Modal = ({ isOpen, onClose, appointment }) => {
-  const { id, activity_name, date, participants, price, status_appointment } =
-    appointment || {};
+    const rdxToken = useSelector(userData);
 
-  const rdxToken = useSelector(userData);
+    const navigate = useNavigate();
+  
+    const { id, activity_name, date, participants, price, status_appointment } =
+      appointment || {};
+  
+    const [appointmentData, setAppointmentData] = useState({
+      date,
+      participants,
+    });
+    
+    const [isEnabled, setIsEnabled] = useState(true);
+  
+    const functionHandler = (e) => {
+      setAppointmentData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value,
+      }));
+    };
 
-  const modifyAppointment = () => {
-    if (status_appointment === "pending") {
-      return (
-        <div className="buttonAppointments">
-          <button
-            className="cancel-Appointment"
-            onClick={() => cancelAppointment(id)}
-          >
-            Cancelar Reserva
-          </button>
-          <button className="modify-Appointment">Modificar Reserva</button>
-        </div>
-      );
-    }  else {
-      return null;
-    }
-  };
+    useEffect(() => {
+        setAppointmentData({
+          date : date ? dayjs(date).format("YYYY-MM-DDTHH:mm") : "",
+          participants,
+        });
+      }, [appointment]);
+      console.log(setAppointmentData, "soy appointments data")
+  
+        const enableEditing = () => {
+      setIsEnabled(!isEnabled);
+    };
+  
+    const sendData = async () => {
+      try {
+        // Verifica si la fecha está vacía
+        if (!appointmentData.date) {
+          console.log("Fecha vacía");
+          return;
+        }
+        console.log(appointmentData, "soy appo")
 
-  const cancelAppointment = async (id_appointment) => {
-    if (rdxToken.credentials !== "" && id_appointment) {
-      const token = rdxToken.credentials.token;
-      const modify = await updateAppointment(
-        {
-          id: id_appointment,
-          status_appointment: "canceled",
-        },
-        token
-      );
-      if (modify) {
-        console.log("modificada");
-        console.log(modify, "modificada")
+        // Verifica si el número de participantes está vacío
+        if (!appointmentData.participants) {
+          console.log("Número de participantes vacío");
+          return;
+        }
+  
+        const token = rdxToken.credentials.token;
+  
+        const newAppointment = await updateAppointment(
+          {
+            id,
+            date: appointmentData.date,
+            participants: appointmentData.participants,
+          },
+          token
+        );
+        if (newAppointment) {
+          navigate("/reservas");
+          console.log("se ha creado la cita");
+        }
+      } catch (error) {
+        console.error("Error al comprobar la disponibilidad", error);
+      } finally {
+        setIsEnabled(true);
       }
-    } else "enviarlo fuera";
-  };
+    };
+  
+    const modifyAppointment = () => {
+      if (status_appointment === "pending") {
+        return (
+          <div className="buttonAppointments">
+            <button
+              className="cancel-Appointment"
+              onClick={() => cancelAppointment(id)}
+            >
+              Cancelar Reserva
+            </button>
 
-  return (
-    <>
-      {isOpen && (
-        <div className="modal-overlay">
-          <div className="modal-container">
-            <div className="modal-header">
-              <h2>Detalles de la Cita</h2>
-              <button className="modal-close" onClick={onClose}>
-                X
-              </button>
-            </div>
-            <div className="modal-content">
-              <p>ID: {id}</p>
-              <p>Actividad: {activity_name}</p>
-              <p>Fecha: {date}</p>
-              <p>Participantes: {participants}</p>
-              <p>Precio: {price}</p>
-            </div>
-            {modifyAppointment()}
+            {isEnabled ? (
+              <Button
+                variant="contained"
+                className="button"
+                onClick={() => sendData()}
+                style={{ textTransform: "none", fontFamily: "" }}
+              >
+                Enviar datos
+              </Button>
+            ): (
+                        <Button
+              variant="contained"
+              className="button"
+              onClick={enableEditing}
+              style={{ textTransform: "none", fontFamily: "" }}
+            >
+             Modificar reserva
+            </Button>
+            )}
           </div>
-        </div>
-      )}
-    </>
-  );
-};
-
-export default Modal;
+        );
+      } else if (status_appointment === "approved") {
+        return (
+          <div className="buttonAppointments">
+            <button
+              className="cancel-Appointment"
+              onClick={() => cancelAppointment(id)}
+            >
+              Cancelar Reserva
+            </button>
+          </div>
+        );
+      } else {
+        return null;
+      }
+    };
+  
+    const cancelAppointment = async (id_appointment) => {
+      if (rdxToken.credentials !== "" && id_appointment) {
+        const token = rdxToken.credentials.token;
+        const modify = await updateAppointment(
+          {
+            id: id_appointment,
+            status_appointment: "canceled",
+          },
+          token
+        );
+        if (modify) {
+          console.log("modificada");
+          console.log(modify, "modificada");
+        }
+      } else console.log("enviarlo fuera");
+    };
+  
+    return (
+      <>
+        {isOpen && (
+          <div className="modal-overlay">
+            <div className="modal-container">
+              <div className="modal-header">
+                <h2>Detalles de la Cita</h2>
+                <button className="modal-close" onClick={onClose}>
+                  X
+                </button>
+              </div>
+              <div className="modal-content">
+                <p>ID: {id}</p>
+                <CustomInput
+                  disabled={""}
+                  label={"Actividad"}
+                  design={"inputDesign"}
+                  type={"text"}
+                  name={"id_activity"}
+                  placeholder={""}
+                  value={activity_name}
+                  maxLength={"25"}
+                  functionProp={functionHandler}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <CustomInput
+                  disabled={!isEnabled}
+                  label={"Fecha"}
+                  design={"inputDesign"}
+                  type={"datetime-local"}
+                  name={"date"}
+                  placeholder={""}
+                  value={appointmentData.date}
+                  functionProp={functionHandler}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <CustomInput
+                  disabled={!isEnabled}
+                  label={"Número de participantes"}
+                  design={"inputDesign"}
+                  type={"number"}
+                  name={"participants"}
+                  placeholder={""}
+                  value={appointmentData.participants}
+                  max={"12"}
+                  functionProp={functionHandler}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <p>Precio: {price}</p>
+              </div>
+              {modifyAppointment()}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  };
+  
+  export default Modal;
