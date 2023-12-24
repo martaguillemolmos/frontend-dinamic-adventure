da65c4881f18c966e25311bec5bfc67e9372fbbe
martaguillemolmos <martetaguillem@gmail.com>
2023-12-24 20:42:08 +0100

    Style: Background Home.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,8 @@ import "./Home.css"
 
 export const Home = () => {
 return (
-    <div className="pagesAuth">
+    <div className="homeDinamic">
         <div className="container">
-            <div className="left"></div>
-            <div className="center">Home AQUÍ</div>
-            <div className="right"></div>
         </div>
     </div>
 )}
